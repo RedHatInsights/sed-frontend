b6748d8af4450cefe048992c323d7b95a82cbbb5
Karel Hala <ch4rlien@gmail.com>
2021-02-03 11:47:30 +0100

    Initial commit

---
+++ config/dev.webpack.config.js
@@ -0,0 +1,22 @@
+const { resolve } = require('path');
+const config = require('@redhat-cloud-services/frontend-components-config');
+const { config: webpackConfig, plugins } = config({
+  rootFolder: resolve(__dirname, '../'),
+  debug: true,
+  https: true,
+  useFileHash: false,
+});
+
+plugins.push(
+  require('@redhat-cloud-services/frontend-components-config/federated-modules')(
+    {
+      root: resolve(__dirname, '../'),
+      useFileHash: false,
+    }
+  )
+);
+
+module.exports = {
+  ...webpackConfig,
+  plugins,
+};
