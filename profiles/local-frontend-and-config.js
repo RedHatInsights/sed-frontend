971466f98c5d43955647e4e8a8f62a39d97d51e8
Karel Hala <khala@redhat.com>
2021-02-04 11:26:07 +0100

    Update paths to match new location

--- profiles/local-frontend-and-config.js
+++ profiles/local-frontend-and-config.js
@@ -11,5 +11,7 @@ routes[`/${SECTION}/${APP_ID}`] = {
 };
 routes[`/beta/apps/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
 routes[`/apps/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
+routes['/beta/config'] = { host: 'http://127.0.0.1:8889/' };
+routes['/config'] = { host: 'http://127.0.0.1:8889/' };
 
 module.exports = { routes };
