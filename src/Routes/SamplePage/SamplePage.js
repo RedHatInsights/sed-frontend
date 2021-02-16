8a769f54c986fa4845009ddf0b3fe54e6155f8a8
Martin Marosi <marvusm.mmi@gmail.com>
2021-02-16 09:14:37 +0100

    Update initial setup

--- src/Routes/SamplePage/SamplePage.js
+++ src/Routes/SamplePage/SamplePage.js
@@ -20,15 +20,6 @@ const SampleComponent = lazy(() =>
   import('../../Components/SampleComponent/sample-component')
 );
 
-import './sample-page.scss';
-
-/**
- * A smart component that handles all the api calls and data needed by the dumb components.
- * Smart components are usually classes.
- *
- * https://reactjs.org/docs/components-and-props.html
- * https://medium.com/@thejasonfile/dumb-components-and-smart-components-e7b33a698d43
- */
 const SamplePage = () => {
   const dispatch = useDispatch();
 
