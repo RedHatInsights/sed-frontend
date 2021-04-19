3a154fd6ef813f5f5648c1c25b4d172189d6ef2b
Karel Hala <khala@redhat.com>
2021-04-19 17:25:17 +0200

    Add tab navigation trough URL

--- src/Components/ConnectLog/index.js
+++ src/Components/ConnectLog/index.js
@@ -1,35 +1,60 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
 import { Modal, Tabs, Tab, TabTitleText } from '@patternfly/react-core';
 import LogsTable from './LogsTable';
 import SystemsTable from './SystemsTable';
-const ConnectLog = ({ isOpen = false, onClose }) => {
+import { useHistory } from 'react-router-dom';
+import pckg from '../../../package.json';
+const { routes: paths } = pckg;
+
+const tabMapper = ['runs', 'systems'];
+
+const ConnectLog = () => {
   const [activeTabKey, setActiveTabKey] = useState(0);
+  const { push, location } = useHistory();
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const activeTab = tabMapper.findIndex(
+      (item) => item === searchParams.get('active_tab')
+    );
+    if (activeTab !== -1) {
+      setActiveTabKey(activeTab);
+    } else {
+      push({
+        pathname: location.pathname,
+        search: new URLSearchParams({
+          active_tab: tabMapper[0],
+        }).toString(),
+      });
+    }
+  }, []);
   return (
     <Modal
       title="Red Hat connect log"
       variant="medium"
-      isOpen={isOpen}
-      onClose={onClose}
+      isOpen={true}
+      onClose={() => push(paths.connector)}
     >
       <Tabs
         activeKey={activeTabKey}
-        onSelect={(_e, tabKey) => setActiveTabKey(tabKey)}
+        onSelect={(_e, tabKey) => {
+          push({
+            pathname: location.pathname,
+            search: new URLSearchParams({
+              active_tab: tabMapper[tabKey],
+            }).toString(),
+          });
+          setActiveTabKey(tabKey);
+        }}
       >
         <Tab eventKey={0} title={<TabTitleText>Runs</TabTitleText>}>
-          {isOpen && <LogsTable />}
+          <LogsTable />
         </Tab>
         <Tab eventKey={1} title={<TabTitleText>Systems</TabTitleText>}>
-          {isOpen && <SystemsTable />}
+          <SystemsTable />
         </Tab>
       </Tabs>
     </Modal>
   );
-};
-
-ConnectLog.propTypes = {
-  isOpen: PropTypes.bool,
-  onClose: PropTypes.func.isRequired,
 };
 
 export default ConnectLog;
