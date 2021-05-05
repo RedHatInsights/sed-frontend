60b5ff0a2d71b3b9d723bc9397fa7b25477a9efa
Martin Marosi <marvusm.mmi@gmail.com>
2021-05-05 10:30:50 +0200

    Use empty state for activation keys tab.

--- src/Components/ActivationKeys/index.js
+++ src/Components/ActivationKeys/index.js
@@ -1,20 +1,32 @@
-import { Title } from '@patternfly/react-core';
 import React from 'react';
-import ActivationKeysTable from './activation-keys-table';
-import NoAccessView from './no-access';
+import {
+  Bullseye,
+  Button,
+  EmptyState,
+  EmptyStateIcon,
+  Title,
+} from '@patternfly/react-core';
+import { LockIcon } from '@patternfly/react-icons';
 
 function ActivationKeys() {
   return (
-    <div>
-      <Title size="2xl" headingLevel="h1">
-        Following content will be in a separate tab. Data is mocked, we
-        don&#39;t know the API endpoint yet.
-      </Title>
-      {/** This will be visible to org admins */}
-      <ActivationKeysTable />
-      {/* This will be visible only to non org admins */}
-      <NoAccessView />
-    </div>
+    <Bullseye>
+      <EmptyState>
+        <EmptyStateIcon icon={LockIcon} />
+        <Title headingLevel="h4" size="lg">
+          This feature is not currently supported
+        </Title>
+        <Button
+          component="a"
+          variant="link"
+          href="https://access.redhat.com/articles/simple-content-access"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Customer portal
+        </Button>
+      </EmptyState>
+    </Bullseye>
   );
 }
 
