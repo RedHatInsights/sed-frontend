5387d2f50de71b42f3435366a2db04d512c90a67
Martin Marosi <marvusm.mmi@gmail.com>
2021-05-05 10:38:32 +0200

    Use consts instead of functions.

--- src/Components/ActivationKeys/index.js
+++ src/Components/ActivationKeys/index.js
@@ -8,26 +8,24 @@ import {
 } from '@patternfly/react-core';
 import { LockIcon } from '@patternfly/react-icons';
 
-function ActivationKeys() {
-  return (
-    <Bullseye>
-      <EmptyState>
-        <EmptyStateIcon icon={LockIcon} />
-        <Title headingLevel="h4" size="lg">
-          This feature is not currently supported
-        </Title>
-        <Button
-          component="a"
-          variant="link"
-          href="https://access.redhat.com/articles/simple-content-access"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Customer portal
-        </Button>
-      </EmptyState>
-    </Bullseye>
-  );
-}
+const ActivationKeys = () => (
+  <Bullseye>
+    <EmptyState>
+      <EmptyStateIcon icon={LockIcon} />
+      <Title headingLevel="h4" size="lg">
+        This feature is not currently supported
+      </Title>
+      <Button
+        component="a"
+        variant="link"
+        href="https://access.redhat.com/articles/simple-content-access"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        Customer portal
+      </Button>
+    </EmptyState>
+  </Bullseye>
+);
 
 export default ActivationKeys;
--- src/Components/ActivationKeys/no-access.js
+++ src/Components/ActivationKeys/no-access.js
@@ -9,22 +9,20 @@ import { LockIcon } from '@patternfly/react-icons';
 import React from 'react';
 import CopyCommand from './copy-command';
 
-function NoAccessView() {
-  return (
-    <Bullseye>
-      <EmptyState>
-        <EmptyStateIcon icon={LockIcon} />
-        <Title headingLevel="h4" size="lg">
-          Activation keys can only be accessed by organization administrators.
-        </Title>
-        <EmptyStateBody>
-          If you already know your organization ID and activation key, you can
-          register systems with RHC.
-        </EmptyStateBody>
-        <CopyCommand />
-      </EmptyState>
-    </Bullseye>
-  );
-}
+const NoAccessView = () => (
+  <Bullseye>
+    <EmptyState>
+      <EmptyStateIcon icon={LockIcon} />
+      <Title headingLevel="h4" size="lg">
+        Activation keys can only be accessed by organization administrators.
+      </Title>
+      <EmptyStateBody>
+        If you already know your organization ID and activation key, you can
+        register systems with RHC.
+      </EmptyStateBody>
+      <CopyCommand />
+    </EmptyState>
+  </Bullseye>
+);
 
 export default NoAccessView;
