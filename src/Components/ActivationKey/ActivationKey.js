aa7119ff54b53f6dfec553e1a3b63bf5e30dd39e
Dmitrii Malyshev <dmalyshe@dmalyshe.remote.csb>
2022-10-03 11:45:38 -0400

    Mock useRouteMatch

--- src/Components/ActivationKey/ActivationKey.js
+++ src/Components/ActivationKey/ActivationKey.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { withRouter, useParams } from 'react-router-dom';
 import pckg from '../../../package.json';
 import Breadcrumbs from '../shared/breadcrumbs';
