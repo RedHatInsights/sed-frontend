191c25c8a489125a31c7bb5f62a33eabf0400582
Dmitrii Malyshev <dmalyshe@dmalyshe.remote.csb>
2022-10-04 17:14:08 -0400

    Do not register activation key route if feature flag is not enabled

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,7 @@ import { Bullseye, Spinner } from '@patternfly/react-core';
 import pckg from '../package.json';
 import Authentication from './Components/Authentication/Authentication';
 import ActivationKey from './Components/ActivationKey/ActivationKey';
+import useFeatureFlag from './hooks/useFeatureFlag';
 const { routes: paths } = pckg;
 
 const Dashboard = lazy(() =>
@@ -12,20 +13,28 @@ const Dashboard = lazy(() =>
 
 const ActivationKeys = lazy(() => import('./Components/ActivationKeys'));
 
-export const Routes = () => (
-  <Suspense
-    fallback={
-      <Bullseye>
-        <Spinner />
-      </Bullseye>
-    }
-  >
-    <Authentication>
-      <Switch>
-        <Route path={paths.activationKey} component={ActivationKey} />
-        <Route path={paths.activationKeys} component={ActivationKeys} />
-        <Route path={paths.connector} component={Dashboard} />
-      </Switch>
-    </Authentication>
-  </Suspense>
-);
+export const Routes = () => {
+  const keyDetailsIsEnabled = useFeatureFlag(
+    'sed-frontend.activationKeysDetailsPage'
+  );
+  return (
+    <Suspense
+      fallback={
+        <Bullseye>
+          <Spinner />
+        </Bullseye>
+      }
+    >
+      <Authentication>
+        <Switch>
+          {keyDetailsIsEnabled && (
+            <Route path={paths.activationKey} component={ActivationKey} />
+          )}
+          <Route path={paths.activationKeys} component={ActivationKeys} />
+
+          <Route path={paths.connector} component={Dashboard} />
+        </Switch>
+      </Authentication>
+    </Suspense>
+  );
+};
