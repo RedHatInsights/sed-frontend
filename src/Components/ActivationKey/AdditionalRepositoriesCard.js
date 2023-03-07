f80dbdea168f1234d14d52b785d23d6358ff9d4c
Brooklyn Lower <blower@redhat.com>
2023-03-07 15:09:04 -0500

    "removed button wrapper, added error handle"

--- src/Components/ActivationKey/AdditionalRepositoriesCard.js
+++ src/Components/ActivationKey/AdditionalRepositoriesCard.js
@@ -15,6 +15,8 @@ import AdditionalRepositoriesTable from '../AdditionalRepositoriesTable';
 import useAvailableRepositories from '../../hooks/useAvailableRepositories';
 import EditAdditionalRepositoriesButton from '../ActivationKey/EditAdditionalRepositoriesButton';
 import EditAdditionalRepositoriesModal from '../Modals/EditAdditionalRepositoriesModal';
+import Loading from '../LoadingState/Loading';
+import Unavailable from '@redhat-cloud-services/frontend-components/Unavailable';
 
 const AdditionalRepositoriesCard = (props) => {
   const { activationKey, isLoading, error } = props;
@@ -32,46 +34,48 @@ const AdditionalRepositoriesCard = (props) => {
       !isEditAdditionalRepositoriesModalOpen
     );
   };
-
-  const ButtonWrapper = () => {
+  const AdditionalReposCard = () => {
     return (
-      <EditAdditionalRepositoriesButton
-        onClick={handleEditAdditionalRepositoriesToggle}
-      />
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            <Title headingLevel="h2"> Additional repositories</Title>
+          </CardTitle>
+        </CardHeader>
+        <CardBody>
+          <TextContent>
+            <Text component={TextVariants.p}>
+              The core repositories for your operating system version, for
+              example BaseOS and AppStream, are always enabled and do not need
+              to be explicitly added to the activation key.
+            </Text>
+            <EditAdditionalRepositoriesButton
+              onClick={handleEditAdditionalRepositoriesToggle}
+            />
+            <EditAdditionalRepositoriesModal
+              title="Additional Repositories"
+              isOpen={isEditAdditionalRepositoriesModalOpen}
+              handleModalToggle={handleEditAdditionalRepositoriesToggle}
+              modalSize="large"
+              repositories={availableRepositories}
+              isLoading={isLoading}
+              error={error}
+            />
+          </TextContent>
+          <AdditionalRepositoriesTable
+            repositories={activationKey.availableRepositories}
+          />
+        </CardBody>
+      </Card>
     );
   };
-
-  return (
-    <Card>
-      <CardHeader>
-        <CardTitle>
-          <Title headingLevel="h2"> Additional repositories</Title>
-        </CardTitle>
-      </CardHeader>
-      <CardBody>
-        <TextContent>
-          <Text component={TextVariants.p}>
-            The core repositories for your operating system version, for example
-            BaseOS and AppStream, are always enabled and do not need to be
-            explicitly added to the activation key.
-          </Text>
-          <ButtonWrapper />
-          <EditAdditionalRepositoriesModal
-            title="Additional Repositories"
-            isOpen={isEditAdditionalRepositoriesModalOpen}
-            handleModalToggle={handleEditAdditionalRepositoriesToggle}
-            modalSize="large"
-            repositories={availableRepositories}
-            isLoading={isLoading}
-            error={error}
-          />
-        </TextContent>
-        <AdditionalRepositoriesTable
-          repositories={activationKey.availableRepositories}
-        />
-      </CardBody>
-    </Card>
-  );
+  if (isLoading && !error) {
+    return <Loading />;
+  } else if (!isLoading && !error) {
+    return AdditionalReposCard();
+  } else {
+    return <Unavailable />;
+  }
 };
 
 AdditionalRepositoriesCard.propTypes = {
