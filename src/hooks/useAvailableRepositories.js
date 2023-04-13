82223bf3e3a000e9fd5a159aa9c5e4c1a43e98c8
Brooklyn Lower <blower@redhat.com>
2023-04-13 16:11:26 -0400

    "Pag offset changes to hook, testing ammends"

--- src/hooks/useAvailableRepositories.js
+++ src/hooks/useAvailableRepositories.js
@@ -1,25 +1,39 @@
 import { useQuery } from 'react-query';
 
-const fetchAdditionalRepositories = async (keyName) => {
+const fetchAdditionalRepositories = async (
+  keyName,
+  offset,
+  allRepositories = []
+) => {
   if (!keyName) {
     return false;
   }
   const token = await window.insights.chrome.auth.getToken();
 
   const response = await fetch(
-    `/api/rhsm/v2/activation_keys/${keyName}/available_repositories?default=Disabled`,
+    `/api/rhsm/v2/activation_keys/${keyName}/available_repositories?default=Disabled&offset=${offset}`,
     {
       headers: { Authorization: `Bearer ${token}` },
     }
   );
 
   const repositoriesData = await response.json();
+  const repositories = repositoriesData.body;
 
-  return repositoriesData.body;
+  if (repositories.length === 0) {
+    return allRepositories;
+  } else {
+    const nextOffset = offset + repositories.length;
+    return fetchAdditionalRepositories(
+      keyName,
+      nextOffset,
+      allRepositories.concat(repositories)
+    );
+  }
 };
 
 const getAvailableRepositories = async (keyName) => {
-  const repositories = await fetchAdditionalRepositories(keyName);
+  const repositories = await fetchAdditionalRepositories(keyName, 0);
   return repositories;
 };
 
