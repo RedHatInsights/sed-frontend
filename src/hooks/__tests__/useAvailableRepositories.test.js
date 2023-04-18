d50d725d0709f6c9e0caa0ba7480ce4ebbea31d1
Brooklyn Lower <101056882+BrooklynLCodes@users.noreply.github.com>
2023-04-18 13:43:10 -0400

    Request to Promote TEAMNADO_4664 Pagination Offset  (#214)
    * "Pag offset changes to hook, testing ammends"
    
    * "prettier ammends"
    
    * Update useAvailableRepositories.js
    
    Token logic added
    
    * Update useAvailableRepositories.js
    
    Removed extra spaces for prettier fix
    
    * Update useAvailableRepositories.js
    
    * Update useAvailableRepositories.js
    
    * "added ARepos mock to AK test"
    
    * "added useChrome logic, repo test ammends"
    
    * "added 100 check"
    
    * "Oopsie fix/change "
    
    * Redundant check removed
    
    * /retest

--- src/hooks/__tests__/useAvailableRepositories.test.js
+++ src/hooks/__tests__/useAvailableRepositories.test.js
@@ -1,32 +1,41 @@
-import { renderHook } from '@testing-library/react-hooks';
-import fetch, { enableFetchMocks } from 'jest-fetch-mock';
-import { createQueryWrapper } from '../../utils/testHelpers';
+import { useQuery } from 'react-query';
 import useAvailableRepositories from '../useAvailableRepositories';
 
-enableFetchMocks();
-
+jest.mock('react-query');
 describe('useAvailableRepositories', () => {
-  it('returns available repositories from the API', async () => {
-    const keyName = [
-      {
-        name: 'A',
-        role: 'role',
-        sla: 'sla',
-        usage: 'usage',
-      },
-    ];
+  const keyName = 'testKey';
 
-    fetch.mockResponseOnce(JSON.stringify({ body: [...keyName] }));
-
-    const { result, waitFor } = renderHook(
-      () => useAvailableRepositories('A'),
-      {
-        wrapper: createQueryWrapper(),
+  beforeEach(() => {
+    useQuery.mockReset();
+  });
+  describe('useAvailableRepositories', () => {
+    test('should fetch available repositories correctly', async () => {
+      const repositories = [];
+      for (let i = 1; i <= 105; i++) {
+        repositories.push({ repositoryId: i, repositoryName: `Repo ${i}` });
       }
-    );
 
-    await waitFor(() => result.current.isSuccess);
+      useQuery.mockReturnValueOnce({
+        isLoading: false,
+        data: repositories,
+      });
 
-    expect(result.current.data).toEqual(keyName);
+      const result = useAvailableRepositories(keyName);
+
+      expect(result.isLoading).toBe(false);
+      expect(result.data).toEqual(repositories);
+    });
+  });
+
+  test('should handle error during fetch correctly', async () => {
+    useQuery.mockReturnValueOnce({
+      isLoading: false,
+      error: new Error('Fetch failed'),
+    });
+
+    const result = useAvailableRepositories(keyName);
+
+    expect(result.isLoading).toBe(false);
+    expect(result.error).toEqual(new Error('Fetch failed'));
   });
 });
--- src/hooks/useAvailableRepositories.js
+++ src/hooks/useAvailableRepositories.js
@@ -1,33 +1,49 @@
 import { useQuery } from 'react-query';
 import useChrome from '@redhat-cloud-services/frontend-components/useChrome';
 
-const fetchAdditionalRepositories = (token) => async (keyName) => {
+const fetchAdditionalRepositories = async (
+  token,
+  keyName,
+  offset = 0,
+  allRepositories = []
+) => {
   if (!keyName) {
     return false;
   }
 
   const response = await fetch(
-    `/api/rhsm/v2/activation_keys/${keyName}/available_repositories?default=Disabled`,
+    `/api/rhsm/v2/activation_keys/${keyName}/available_repositories?default=Disabled&offset=${offset}`,
     {
       headers: { Authorization: `Bearer ${await token}` },
     }
   );
 
+  if (!response.ok) {
+    throw new Error('Failed to fetch repositories');
+  }
+
   const repositoriesData = await response.json();
+  const repositories = repositoriesData.body;
 
-  return repositoriesData.body;
-};
-
-const getAvailableRepositories = (token) => async (keyName) => {
-  const repositories = await fetchAdditionalRepositories(token)(keyName);
-  return repositories;
+  if (repositories.length < 100) {
+    return allRepositories.concat(repositories);
+  } else {
+    const nextOffset = offset + repositories.length;
+    return fetchAdditionalRepositories(
+      token,
+      keyName,
+      nextOffset,
+      allRepositories.concat(repositories)
+    );
+  }
 };
 
 const useAvailableRepositories = (keyName) => {
   const chrome = useChrome();
+  const token = chrome?.auth?.getToken();
 
   return useQuery(`activation_key_${keyName}_available_repositories`, () =>
-    getAvailableRepositories(chrome?.auth?.getToken())(keyName)
+    fetchAdditionalRepositories(token, keyName)
   );
 };
 
