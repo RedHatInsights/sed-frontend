5e179b76114adfbbc295f7fb85364e7a8c9e966e
Luke Green <lgreen@redhat.com>
2023-07-21 12:01:42 -0500

    Unset release version
    This allows us to unset the release version for an activation key. It is
    dependent on RHSM-API adding support for it, so don't merge this until
    that is ready. It can be reviewed now though.

--- src/Components/Forms/EditReleaseVersionForm.js
+++ src/Components/Forms/EditReleaseVersionForm.js
@@ -29,16 +29,13 @@ export const EditReleaseVersionForm = ({
     <FormSelectOption value={version} label={version} key={i} />
   ));
 
-  if (!activationKey.releaseVersion) {
-    options.push(
-      <FormSelectOption
-        value=""
-        label="Not defined"
-        key={releaseVersions?.length}
-        isDisabled
-      />
-    );
-  }
+  options.push(
+    <FormSelectOption
+      value=""
+      label="Not defined"
+      key={releaseVersions?.length}
+    />
+  );
 
   const submitForm = () => {
     mutate(
