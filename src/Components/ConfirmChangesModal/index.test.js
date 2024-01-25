1faaa6d7f0e5c4ca4a4936aaa32f6465b7589b76
Lior Keren <lkeren@redhat.com>
2024-01-25 14:33:22 +0200

    (feat): Refactor removed cypress test to RTL

--- src/Components/ConfirmChangesModal/index.test.js
+++ src/Components/ConfirmChangesModal/index.test.js
@@ -1,12 +1,23 @@
-import { render } from '@testing-library/react';
-import ConfirmChangesModal from '.';
 import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import ConfirmChangesModal from '.';
+
+jest.mock(
+  '@redhat-cloud-services/frontend-components-utilities/interceptors',
+  () => ({
+    __esModule: true,
+    useAxiosWithPlatformInterceptors: () => require('axios'),
+  })
+);
 
 describe('ConfirmChangesModal', () => {
   it('renders correctly', () => {
-    const { getByText } = render(
-      <ConfirmChangesModal isOpen systemsCount={2} />
-    );
-    expect(getByText('Confirm changes')).toBeInTheDocument();
+    const handleCancel = jest.fn();
+    const handleConfirm = jest.fn();
+    render(<ConfirmChangesModal isOpen systemsCount={2} handleCancel={handleCancel} handleConfirm={handleConfirm} />);
+    expect(screen.getByText('View playbook')).toBeInTheDocument();
+    expect(screen.getAllByText('Confirm changes')).toHaveLength(2);
+    expect(screen.getAllByText('Confirm changes')[0]).toBeInTheDocument();
   });
 });
