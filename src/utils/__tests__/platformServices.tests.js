f5c51819abb5a9aa89fba481fcefea35e1f4c113
Lior Keren <lkeren@redhat.com>
2024-01-30 12:58:31 +0200

    (feat): Refactor test
    - Fix wrong getUser mock
    - Remove beforeEach

--- src/utils/__tests__/platformServices.tests.js
+++ src/utils/__tests__/platformServices.tests.js
@@ -7,26 +7,18 @@ jest.mock('@redhat-cloud-services/frontend-components/useChrome', () =>
   jest.fn()
 );
 
-beforeEach(() => {
-  useChrome.mockImplementation(() => ({
-    auth: {
-      getUser: () => {},
-    },
-  }));
-});
-
 describe('Authenticate User method', () => {
   it('should return a promise with user data', async () => {
     useChrome.mockImplementation(() => ({
       auth: {
-        getUser: () => ({
+        getUser: () => Promise.resolve(({
           identity: {
             account_number: 1,
             internal: {
               org_id: 1,
             },
           },
-        }),
+        })),
       },
     }));
 
@@ -49,26 +41,13 @@ describe('Authenticate User method', () => {
   it('should throw an error if rejected', async () => {
     useChrome.mockImplementation(() => ({
       auth: {
-        getUser: () => {
-          throw new Error('Error getting user');
+          getUser: () => Promise.reject(new Error('Error getting user')),
         },
-      },
-    }));
-
-    // await waitFor(() => expect(renderHook(() => useAuthenticateUser(), {
-    //   wrapper: createQueryWrapper(),
-    // })).toThrow(Error('Error authenticating user: Error getting user')));
-
-    const { result } = renderHook(() => useAuthenticateUser(), {
-      wrapper: createQueryWrapper(),
-    });
-
-    await waitFor(() => expect(result.current.isError).toBe(true));
-
-    // expect(result).toThrow(Error('Error authenticating user: Error getting user'));
-
-    expect(result.current.error.message).toEqual(
-      'Error authenticating user: Error getting user'
+      }),
     );
+
+    expect(() => renderHook(() => useAuthenticateUser(), {
+      wrapper: createQueryWrapper(),
+    }).toThrow(Error('Error getting user')));
   });
 });
