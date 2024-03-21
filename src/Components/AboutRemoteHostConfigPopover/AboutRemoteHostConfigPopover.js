a6131bf39558580de238be6b2091abd97188b9a4
Muslimjon <59481011+mkholjuraev@users.noreply.github.com>
2024-03-21 17:06:58 +0100

    chore(RHINENG-7106): update doc links with 1-latest (#274)

--- src/Components/AboutRemoteHostConfigPopover/AboutRemoteHostConfigPopover.js
+++ src/Components/AboutRemoteHostConfigPopover/AboutRemoteHostConfigPopover.js
@@ -50,7 +50,7 @@ const ConnectSystemsModal = () => {
             target="_blank"
             rel="noopener noreferrer"
             href={
-              'https://access.redhat.com/documentation/en-us/red_hat_insights/2023/html/remote_host_configuration_and_management/index'
+              'https://access.redhat.com/documentation/en-us/red_hat_insights/1-latest/html/remote_host_configuration_and_management/index'
             }
           >
             Connecting with Red Hat
