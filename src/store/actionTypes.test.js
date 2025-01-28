d73ae8dea7872c1ce8d0fa9f5be3d264f43e21f6
leSamo <soleksak@redhat.com>
2025-01-28 14:18:24 +0100

    Remove associated redux actions

--- src/store/actionTypes.test.js
+++ src/store/actionTypes.test.js
@@ -1,7 +1,6 @@
 import {
   GET_CONNECTED_HOSTS,
   GET_CURR_STATE,
-  GET_LOG,
   SET_CURR_STATE,
 } from './actionTypes';
 
@@ -9,7 +8,6 @@ describe('actionTypes', () => {
   test('defined', () => {
     expect(GET_CONNECTED_HOSTS).toBe('GET_CONNECTED_HOSTS');
     expect(GET_CURR_STATE).toBe('GET_CURR_STATE');
-    expect(GET_LOG).toBe('GET_LOG');
     expect(SET_CURR_STATE).toBe('SET_CURR_STATE');
   });
 });
