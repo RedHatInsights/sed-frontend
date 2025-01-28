e49e8487ebf0418ea7233a9a6d167b9d79d51297
leSamo <soleksak@redhat.com>
2025-01-28 15:01:05 +0100

    chore(RHINENG-15358): Cleanup and consolidate frontend repositories

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,5 @@ require.extensions['.css'] = () => undefined;
 
 module.exports = {
   presets: ['@babel/env', '@babel/react'],
-  plugins: [
-    '@babel/plugin-transform-runtime',
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-proposal-class-properties',
-    'lodash',
-  ],
+  plugins: ['@babel/plugin-transform-runtime', 'lodash'],
 };
