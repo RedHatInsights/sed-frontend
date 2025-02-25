87083f5976c035952dc47c4af30afa8882fb7017
opacut <opacut@redhat.com>
2025-02-25 14:05:12 +0100

    chore(RHINENG-16213) fix Cypress coverage and collection (#379)
    * fix Cypress coverage and collection
    
    * update gitignore

--- config/cypress.webpack.config.js
+++ config/cypress.webpack.config.js
@@ -6,4 +6,15 @@ const { config: webpackConfig, plugins } = config({
 module.exports = {
   ...webpackConfig,
   plugins,
+  module: {
+    ...webpackConfig.module,
+    rules: [
+      ...webpackConfig.module.rules,
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /(node_modules|bower_components)/i,
+        use: ['babel-loader'],
+      },
+    ],
+  },
 };
