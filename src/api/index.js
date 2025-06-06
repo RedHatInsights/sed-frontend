1def917122276c357a4de30fa9d5f479ff00c253
Hugo Hezel <hhezel@redhat.com>
2025-06-06 08:15:28 +0200

    feat(RHINENG-15924): Update javascript client to the latest version

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,14 @@
 export const CONNECTOR_API_BASE = '/api/config-manager/v2';
 
 import { useAxiosWithPlatformInterceptors } from '@redhat-cloud-services/frontend-components-utilities/interceptors';
-import { DefaultApi } from '@redhat-cloud-services/config-manager-client';
+import { ConfigManagerClient } from '@redhat-cloud-services/config-manager-client/api';
 import { useEffect, useRef, useState } from 'react';
 
 export * from './inventory';
 
 export const useConfigApi = () => {
   const axiosInstance = useAxiosWithPlatformInterceptors();
-  return new DefaultApi(undefined, CONNECTOR_API_BASE, axiosInstance);
+  return ConfigManagerClient(CONNECTOR_API_BASE, axiosInstance);
 };
 
 export const useGetPlaybookPreview = (profileId) => {
